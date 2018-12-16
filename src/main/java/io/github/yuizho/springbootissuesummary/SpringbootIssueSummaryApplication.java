c8b627f204397029adc94ac432cf4e29ae8a9ba6
yuizho <yuizho3@gmail.com>
2018-12-16 09:22:01 +0900

    add cache feature to improve performance

--- src/main/java/io/github/yuizho/springbootissuesummary/SpringbootIssueSummaryApplication.java
+++ src/main/java/io/github/yuizho/springbootissuesummary/SpringbootIssueSummaryApplication.java
@@ -2,8 +2,10 @@ package io.github.yuizho.springbootissuesummary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringbootIssueSummaryApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/github/yuizho/springbootissuesummary/infrastructure/api/IssuesApiClient.java
+++ src/main/java/io/github/yuizho/springbootissuesummary/infrastructure/api/IssuesApiClient.java
@@ -5,6 +5,8 @@ import io.github.yuizho.springbootissuesummary.domain.models.Issue;
 import io.github.yuizho.springbootissuesummary.domain.collections.Issues;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -16,13 +18,15 @@ import java.util.List;
 public class IssuesApiClient extends AbstractApiClient implements IssuesFetcher {
     private static final String uri = "https://api.github.com/repos/spring-projects/spring-boot/issues?page=1&per_page=10";
 
+    @Cacheable(value = "issues")
     @Override
     public Issues fetchIssues() {
         HttpResponse<String> response = fetch();
         return convertResultToDomain(response.body());
     }
 
-    // TODO: ここで有効期限指定して、キャッシュに突っ込む
+    // TODO: ここで有効期限指定したい。。。Taskとかで定期的に消さないとだめかな？
+    @CachePut(value = "issues")
     Issues convertResultToDomain(String body) {
         JSONArray jsonArray = new JSONArray(body);
 
