8883017c6cd554809faf788c63136ba0c1f6ebd4
yuizho <yuizho3@gmail.com>
2018-12-18 19:57:14 +0900

    modify IssuesRestCacheJob class
    to improve relation of the classes

--- src/main/java/io/github/yuizho/springbootissuesummary/infrastructure/rest/jobs/IssuesRestCacheJob.java
+++ src/main/java/io/github/yuizho/springbootissuesummary/infrastructure/rest/jobs/IssuesRestCacheJob.java
@@ -1,8 +1,8 @@
 package io.github.yuizho.springbootissuesummary.infrastructure.rest.jobs;
 
+import io.github.yuizho.springbootissuesummary.domain.adopters.IssuesFetcher;
 import io.github.yuizho.springbootissuesummary.domain.collections.Issues;
 import io.github.yuizho.springbootissuesummary.infrastructure.DataSourceProperties;
-import io.github.yuizho.springbootissuesummary.infrastructure.rest.adopters.IssuesRestFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -10,13 +10,20 @@ import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
-public class IssuesRestCacheJob extends IssuesRestFetcher implements CacheJob {
+public class IssuesRestCacheJob implements CacheJob {
     @Autowired
     private CacheManager cacheManager;
 
     @Autowired
     private DataSourceProperties dataSourceProperties;
+
+    @Autowired
+    private Map<String, IssuesFetcher> issuesFetchers;
+
+    private static final String DATA_SOURCE_TYPE_REST = "REST";
 
     /**
      * this method is executed as scheduled job
@@ -24,13 +31,13 @@ public class IssuesRestCacheJob extends IssuesRestFetcher implements CacheJob {
     @Override
     @Scheduled(fixedDelay = (1000 * 60 * 3))
     public void refreshCache() {
-        if ("REST".equals(dataSourceProperties.getType())) {
-            Cache issuesCache = cacheManager.getCache(DOMAIN_NAME);
+        if (DATA_SOURCE_TYPE_REST.equals(dataSourceProperties.getType())) {
+            Cache issuesCache = cacheManager.getCache(IssuesFetcher.DOMAIN_NAME);
             if (issuesCache == null) {
                 return;
             }
             issuesCache.clear();
-            Issues issues = fetchIssues();
+            Issues issues= issuesFetchers.get(IssuesFetcher.DOMAIN_NAME + DATA_SOURCE_TYPE_REST).fetchIssues();
             issuesCache.put(SimpleKey.EMPTY, issues);
         }
     }
