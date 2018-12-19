cd256f962cf42f80e50376a0e7f88a4723620129
yuizho <yuizho3@gmail.com>
2018-12-19 21:46:52 +0900

    add RestApiClient test class

---
+++ src/test/java/io/github/yuizho/springbootissuesummary/infrastructure/rest/RestApiClientTest.java
@@ -0,0 +1,134 @@
+package io.github.yuizho.springbootissuesummary.infrastructure.rest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.junit.MockServerRule;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+public class RestApiClientTest {
+    @Rule
+    public MockServerRule mockServerRule = new MockServerRule(this, 1081);
+
+    private MockServerClient mockServerClient;
+
+    @Test
+    public void testGet() throws IOException, InterruptedException {
+        mockServerClient
+                .when(
+                        request()
+                                .withMethod("GET")
+                                .withPath("/test")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withBody("OK")
+                );
+
+        RestApiClient restApiClient = new RestApiClient();
+        HttpResponse<String> actual = restApiClient.get(URI.create("http://localhost:1081/test"));
+
+        assertThat(actual.statusCode()).isEqualTo(200);
+        assertThat(actual.body()).isEqualTo("OK");
+    }
+
+    @Test
+    public void testDelete() throws IOException, InterruptedException {
+        mockServerClient
+                .when(
+                        request()
+                                .withMethod("DELETE")
+                                .withPath("/test")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                );
+
+        RestApiClient restApiClient = new RestApiClient();
+        HttpResponse<String> actual = restApiClient.delete(URI.create("http://localhost:1081/test"));
+
+        assertThat(actual.statusCode()).isEqualTo(200);
+    }
+
+    @Test
+    public void testPost() throws IOException, InterruptedException {
+        mockServerClient
+                .when(
+                        request()
+                                .withMethod("POST")
+                                .withPath("/test")
+                                .withBody("{username: 'foo'}")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withBody("OK")
+                );
+
+        RestApiClient restApiClient = new RestApiClient();
+        HttpResponse<String> actual
+                = restApiClient.post(
+                        URI.create("http://localhost:1081/test"),
+                        HttpRequest.BodyPublishers.ofString("{username: 'foo'}"));
+
+        assertThat(actual.statusCode()).isEqualTo(200);
+        assertThat(actual.body()).isEqualTo("OK");
+    }
+
+    @Test
+    public void testPut() throws IOException, InterruptedException {
+        mockServerClient
+                .when(
+                        request()
+                                .withMethod("PUT")
+                                .withPath("/test")
+                                .withBody("{username: 'foo'}")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withBody("OK")
+                );
+
+        RestApiClient restApiClient = new RestApiClient();
+        HttpResponse<String> actual
+                = restApiClient.put(
+                URI.create("http://localhost:1081/test"),
+                HttpRequest.BodyPublishers.ofString("{username: 'foo'}"));
+
+        assertThat(actual.statusCode()).isEqualTo(200);
+        assertThat(actual.body()).isEqualTo("OK");
+    }
+
+    @Test
+    public void test404ErrorIsReturned() throws IOException, InterruptedException {
+        mockServerClient
+                .when(
+                        request()
+                                .withMethod("GET")
+                                .withPath("/test")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(404)
+                );
+
+        RestApiClient restApiClient = new RestApiClient();
+
+        assertThatThrownBy(() -> restApiClient.get(URI.create("http://localhost:1081/test")))
+                .isInstanceOfSatisfying(IOException.class, e -> {
+                    assertThat(e.getMessage()).isEqualTo("the status code responded by external API is 404.");
+                });
+    }
+}
